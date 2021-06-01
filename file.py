47494ec59d77dce0f2822141fa4a5b28a22e3531
Calixto Nascimento <calixtoalbea@gmail.com>
2021-06-01 15:54:29 -0300

    IF

--- file.py
+++ file.py
@@ -1,3 +1,6 @@
+"""
+Aula Variáveis
+
 var1 = 1  # Variável Inteira
 var2 = 1.1  # Variável Float
 var3 = "Eu sou uma string"  # Variável String
@@ -5,3 +8,14 @@ var4 = True  # Verdadeiro
 var4 = False  # Falso
 
 print(var1, var2, var3)
+
+"""
+
+# Condicional If
+x = 1
+y = 1000000000
+
+if x > y:
+    print("X é maior que y")
+if y > x:
+    print("Y é maior que x")
