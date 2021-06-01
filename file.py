e9009c30260796f018b3c285853665ed3ef236aa
Calixto Nascimento <calixtoalbea@gmail.com>
2021-06-01 15:44:10 -0300

    Variáveis Python

--- file.py
+++ file.py
@@ -1,1 +1,7 @@
-print(10 % 3)
+var1 = 1  # Variável Inteira
+var2 = 1.1  # Variável Float
+var3 = "Eu sou uma string"  # Variável String
+var4 = True  # Verdadeiro
+var4 = False  # Falso
+
+print(var1, var2, var3)
